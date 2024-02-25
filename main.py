93f2abf8620441e5f95d2bf774626bb1af7f92ad
wingie125 <146167220+wingie125@users.noreply.github.com>
2024-02-25 23:41:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from coffee_maker import CoffeeMaker
+from menu import Menu
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    option = menu.get_items()
+    choice = input(f"What would you like ? {option}  ")
+    if choice == "of":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
